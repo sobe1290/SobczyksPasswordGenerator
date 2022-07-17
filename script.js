33f4230ce332ab60d65b0bc8f1cc5074fa760190
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-07-16 22:23:14 -0700

    added link to wirefram in Readme, added more JS

--- script.js
+++ script.js
@@ -5,26 +5,50 @@ const lowerCaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
 const upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+
 //console.log(specialCharacters.length);
 //console.log(lowerCaseAlphabet.length);
 //console.log(upperCaseAlphabet.length);
 //console.log(numbers.length);
 
 const randomSpecialCharacter = Math.floor(Math.random() * specialCharacters.length);
-//console.log(specialCharacters[randomSpecialCharacter]);
-
 const randomLowerCaseAlphabet = Math.floor(Math.random() * lowerCaseAlphabet.length);
-//console.log(lowerCaseAlphabet[randomLowerCaseAlphabet]);
-
 const randomUpperCaseAlphabet = Math.floor(Math.random() * upperCaseAlphabet.length);
-//console.log(upperCaseAlphabet[randomUpperCaseAlphabet]);
-
 const randomNumbers = Math.floor(Math.random() * numbers.length);
+
+//console.log(specialCharacters[randomSpecialCharacter]);
+//console.log(lowerCaseAlphabet[randomLowerCaseAlphabet]);
+//console.log(upperCaseAlphabet[randomUpperCaseAlphabet]);
 //console.log(numbers[randomNumbers]);
 
-function randomCombiner() {
-  console.log(`${specialCharacters[randomSpecialCharacter]}${lowerCaseAlphabet[randomLowerCaseAlphabet]}${upperCaseAlphabet[randomUpperCaseAlphabet]}${numbers[randomNumbers]}`)
+const randomCharactersArray = [`${randomSpecialCharacter}${randomLowerCaseAlphabet}${randomUpperCaseAlphabet}${randomNumbers}`]
+
+
+
+if (selectedCharacters = true) {
+  selectedCharacters = randomSpecialCharacter;
+  growingString.push(randomSpecialCharacter);
 }
+if (selectedNumbers = true) {
+  selectedNumbers = randomNumbers;
+  growingString.push(randomNumbers);
+}
+if (selectedLower = true) {
+  selectedLower = randomLowerCaseAlphabet;
+  growingString.push(randomLowerCaseAlphabet);
+}
+if (selectedUpper = true) {
+  selectedUpper = randomUpperCaseAlphabet;
+  growingString.push(randomUpperCaseAlphabet);
+}
+
+
+const growingString = []
+
+
+//function randomCombiner() {
+  //console.log(`${specialCharacters[randomSpecialCharacter]}${lowerCaseAlphabet[randomLowerCaseAlphabet]}${upperCaseAlphabet[randomUpperCaseAlphabet]}${numbers[randomNumbers]}`)
+//}
 
 randomCombiner()
 
@@ -33,6 +57,11 @@ randomCombiner()
 
 
 // Get references to the #generate element
+var selectedCharacters = document.querySelector("#specialCharacter");
+var selectedNumbers = document.querySelector("#numbers");
+var selectedLower = document.querySelector("#lowerCase");
+var selectedUpper = document.querySelector("#upperCase");
+var userSelectedLength = document.querySelector("#length")
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
