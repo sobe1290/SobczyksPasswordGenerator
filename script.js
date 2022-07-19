d67023bbc159b0653a9128dc6f21da7d59750623
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-07-19 12:37:15 -0700

    added more js, debugging

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-// Assignment code here
+// These are the references to the html selectors for password length
+/*var selectedCharacters = document.querySelector("#specialCharacter");
+var selectedNumbers = document.querySelector("#numbers");
+var selectedLower = document.querySelector("#lowerCase");
+var selectedUpper = document.querySelector("#upperCase");
+var userSelectedLength = document.querySelector("#length");*/
+var generateBtn = document.querySelector("#generate");
 
-const specialCharacters = [" ", "!" ,"#" ,"$" ,"%" ,"&" ,"'" ,"(" ,")" ,"*" ,"+" , "-",".", "/", ":",";", "<", "=", ">", "?", "@" ,"[", "]", "^", "_", "`", "{" ,"|" ,"}" ,"~"];
-const lowerCaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//This section defines the base strings of all available characters.
+var specialCharacters = [" ", "!" ,"#" ,"$" ,"%" ,"&" ,"'" ,"(" ,")" ,"*" ,"+" , "-",".", "/", ":",";", "<", "=", ">", "?", "@" ,"[", "]", "^", "_", "`", "{" ,"|" ,"}" ,"~"];
+var lowerCaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
-//console.log(specialCharacters.length);
-//console.log(lowerCaseAlphabet.length);
-//console.log(upperCaseAlphabet.length);
-//console.log(numbers.length);
+//This section takes one character at random from the selected string.
+var randomSpecialCharacter = Math.floor(Math.random() * specialCharacters.length);
+var randomLowerCaseAlphabet = Math.floor(Math.random() * lowerCaseAlphabet.length);
+var randomUpperCaseAlphabet = Math.floor(Math.random() * upperCaseAlphabet.length);
+var randomNumbers = Math.floor(Math.random() * numbers.length);
 
-const randomSpecialCharacter = Math.floor(Math.random() * specialCharacters.length);
-const randomLowerCaseAlphabet = Math.floor(Math.random() * lowerCaseAlphabet.length);
-const randomUpperCaseAlphabet = Math.floor(Math.random() * upperCaseAlphabet.length);
-const randomNumbers = Math.floor(Math.random() * numbers.length);
+console.log(randomSpecialCharacter)
 
-//console.log(specialCharacters[randomSpecialCharacter]);
-//console.log(lowerCaseAlphabet[randomLowerCaseAlphabet]);
-//console.log(upperCaseAlphabet[randomUpperCaseAlphabet]);
-//console.log(numbers[randomNumbers]);
+selectedCharacters = true;
+selectedNumber = true;
+selectedLower= true;
+selectedCharacters= true;
+userSelectedLength = 12;
 
+//This section has the if statements that if an item is selected on the form, then a random character from that string will be added to growingString
+var growingString = []
 
 if (selectedCharacters = true) {
   selectedCharacters = randomSpecialCharacter;
@@ -40,35 +48,35 @@ if (selectedUpper = true) {
 }
 
 
-const growingString = []
 
+//These are the if statements that control the length of the password.
 
-//function randomCombiner() {
-  //console.log(`${specialCharacters[randomSpecialCharacter]}${lowerCaseAlphabet[randomLowerCaseAlphabet]}${upperCaseAlphabet[randomUpperCaseAlphabet]}${numbers[randomNumbers]}`)
-//}
+for (growingString.length; growingString.length <8; growingString.length++){
+  growingString.push(selectedRandomPool)
+}
+for (growingString.length; growingString.length < userSelectedLength; growingString.length++){
+    growingString.push(selectedRandomPool)
+}
+/*for (growingString.length; growingString.length >128; growingString.length--){
+    growingString.push(selectedRandomPool)
+}*/
 
-randomCombiner()
+//This is an additional side pool created to pull from if additional characters are needed to add to the password length.
 
-//const scrambledRandomCombined = 
+var selectedRandomPool = Math.floor(Math.random()*growingString.length);
 
-
-
-// Get references to the #generate element
-var selectedCharacters = document.querySelector("#specialCharacter");
-var selectedNumbers = document.querySelector("#numbers");
-var selectedLower = document.querySelector("#lowerCase");
-var selectedUpper = document.querySelector("#upperCase");
-var userSelectedLength = document.querySelector("#length")
-var generateBtn = document.querySelector("#generate");
+console.log (selectedRandomPool) // this was just put here to test in console
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  console.log(growingString)
+    passwordText.value = growingString;
 
 }
 
+writePassword()  //this was just put here to test in console
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
