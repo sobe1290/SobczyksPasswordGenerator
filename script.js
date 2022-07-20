d9efb687991785e201b818dfd52b00e23f15aee1
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-07-19 17:54:46 -0700

    Added slider for user input, js not working

--- script.js
+++ script.js
@@ -12,70 +12,47 @@ var selectedLower= true;
 var selectedUpper= true;
 var userSelectedLength = 15;
 
+var slider = document.getElementById("myRange");
+var output = document.getElementById("selectedLength");
+output.innerHTML = slider.value; // Display the default slider value
 
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
 
-
-//This section takes one character at random from the selected string.
-
-function generatePassword () {
   
-  //This section defines the base strings of all available characters.
   var specialCharacters = [" ", "!" ,"#" ,"$" ,"%" ,"&" ,"'" ,"(" ,")" ,"*" ,"+" , "-",".", "/", ":",";", "<", "=", ">", "?", "@" ,"[", "]", "^", "_", "`", "{" ,"|" ,"}" ,"~"];
   var lowerCaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var growingString = [];
-  var selectedRandomPool = [];
-
-  var randomSpecialCharacter = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  var growingString ="";
+  var randomSpecialCharacter = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];    
   var randomLowerCaseAlphabet = lowerCaseAlphabet[Math.floor(Math.random() * lowerCaseAlphabet.length)];
   var randomUpperCaseAlphabet = upperCaseAlphabet[Math.floor(Math.random() * upperCaseAlphabet.length)];
   var randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
 
-  //This section has the if statements that if an item is selected on the form, then a random character from that string will be added to growingString
-  
-  
-  function addRandomSpecialChar () {
-    selectedRandomPool.push(randomSpecialCharacter);
-  }
-  function addRandomNumbers () {
-    selectedRandomPool.push(randomNumbers);
-  }
-  function addRandomLowerCase () {
-    selectedRandomPool.push(randomLowerCaseAlphabet);
-  }
-  function addRandomUpperCase () {
-    selectedRandomPool.push(randomUpperCaseAlphabet);
-  }
-  
+  console.log (growingString)
+
+function generatePassword () {
   if (selectedCharacters === true) {
-    growingString.push(randomSpecialCharacter); 
-    addRandomSpecialChar (); 
+    growingString.concat("", specialCharacters[Math.floor(Math.random() * specialCharacters.length)]); 
   };
   if (selectedNumbers === true) {
-    growingString.push(randomNumbers);
-    addRandomNumbers ();
+    growingString.concat("", numbers[Math.floor(Math.random() * numbers.length)]);
   };
   if (selectedLower === true) {
-    growingString.push(randomLowerCaseAlphabet);
-    addRandomLowerCase ();
+    growingString.concat("", lowerCaseAlphabet[Math.floor(Math.random() * lowerCaseAlphabet.length)]);
   };
   if (selectedUpper === true) {
-    growingString.push(randomUpperCaseAlphabet);
-    addRandomUpperCase ();
+    growingString.concat("", numbers[Math.floor(Math.random() * numbers.length)]);
   };
-  //These are the if statements that control the length of the password.
-
-  //This is an additional side pool created to pull from if additional characters are needed to add to the password length.
- 
-
-  
 
     function characterExpander() {
-      for (growingString; growingString.length <8; growingString+selectedRandomPool){
+      for (growingString; growingString.length <8; growingString++){
       };
 
-      for (growingString; growingString.length < userSelectedLength; growingString+selectedRandomPool){
+      for (growingString; growingString.length < userSelectedLength; growingString++){
       };
       /*for (growingString.length; growingString.length >128; growingString.length--){
           growingString.push(selectedRandomPool)
@@ -83,9 +60,7 @@ function generatePassword () {
     }
 
   characterExpander();
-  console.log(growingString);
-  console.log(growingString.length);
-  return growingString.join(' ');
+
 }
 
 
