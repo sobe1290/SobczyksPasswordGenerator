667cfca83c961f5f25cf19682b2eec3db7287e46
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-07-20 17:19:42 -0700

    started adding the prompts

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ var selectedLower = document.querySelector("#lowerCase");
 var selectedUpper = document.querySelector("#upperCase");
 var userSelectedLength = document.querySelector("#length");*/
 var generateBtn = document.querySelector("#generate");
+var tryAgainBtn = document.querySelector("#tryAgain");
 
-var selectedCharacters = false;
-var selectedNumbers = true;
-var selectedLower= false;
-var selectedUpper= true;
-var userSelectedLength= 15;
+var selectedCharacters;
+var selectedNumbers;
+var selectedLower;
+var selectedUpper;
+var userSelectedLength;
 
 
   
@@ -19,10 +20,51 @@ var lowerCaseAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
 var upperCaseAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var growingString;
-var growingStringByLength= [];
+var finalPassword =[];
+
 
 
 function generatePassword () {
+
+  userSelectedLength = parseInt(prompt("Please enter the length of your desired password. Enter a number between 8 and 128 to continue."));
+    if (!userSelectedLength) {
+      alert("You must enter a number between 8 and 128");
+    } else if (userSelectedLength <8 || userSelectedLength >128) {
+      userSelectedLength = parseInt(prompt("You must enter a number between 8 and 128"));
+    } 
+
+    console.log(userSelectedLength)
+    
+    specialCharacters = confirm("Do you want to use special characters?");
+      if (!specialCharacters) {
+        specialCharacters = false;
+      } else if (specialCharacters) {
+        specialCharacters = true;
+      }
+      
+    lowerCaseAlphabet = confirm("Do you want to use lower case letters?");
+      if (!lowerCaseAlphabet) {
+        lowerCaseAlphabet = false;
+      } else if (lowerCaseAlphabet) {
+        lowerCaseAlphabet = true;
+      }
+
+
+    upperCaseAlphabet = confirm("Do you want upper case letters?");
+      if (!upperCaseAlphabet) {
+        upperCaseAlphabet = false;
+      } else if (upperCaseAlphabet) {
+        upperCaseAlphabet = true;
+      }
+
+    numbers = confirm("do you want numbers");
+      if (!numbers) {
+        numbers= false;
+      } else if (numbers) {
+        numbers = true;
+      }
+  
+
   if (selectedCharacters === true && selectedNumbers === true && selectedLower === true && selectedUpper === true) {
     growingString = specialCharacters.concat(lowerCaseAlphabet, upperCaseAlphabet, numbers); 
   };
@@ -69,7 +111,20 @@ function generatePassword () {
     growingString = upperCaseAlphabet; 
   };
 
-  console.log(growingString)
+  
+  for (var i=0; i < userSelectedLength; i++) {
+    var growingStringByLength = growingString[Math.floor(Math.random() * growingString.length)];
+    finalPassword.push(growingStringByLength);
+    
+  }
+  
+function join (){
+  finalPassword = finalPassword.join("")
+}
+
+join ()
+
+return finalPassword;
 }
 
 
@@ -87,4 +142,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+const reloadtButton = document.querySelector("#reload");
+// Reload everything:
+function reload() {
+  reload = location.reload();
+}
+// Event listeners for reload
+tryAgainBtn.addEventListener("click", reload, false);
 
